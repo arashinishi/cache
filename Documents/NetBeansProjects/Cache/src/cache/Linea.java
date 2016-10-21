ba3e1f7fd46eb5635c5866984f893f0a6e364337
Victor Reyes Medina <victor.reyes.medina@gmail.com>
2016-10-21 20:33:18 -0300

    Linea direct map & fully v2

--- Documents/NetBeansProjects/Cache/src/cache/Linea.java
+++ Documents/NetBeansProjects/Cache/src/cache/Linea.java
@@ -6,20 +6,16 @@ public class Linea {
     private int offsetSize;
     private int indexSize;
     private int tagSize;
-    private int[] linea;
+    public int[] linea;
     
-    public Linea(boolean isWriteBack, boolean isDirect, boolean isFully, boolean isAllocate, boolean isSplit, int set, int blockSize, int cacheSize)
+    public Linea(boolean isDirect, boolean isFully, int set, int blockSize, int cacheSize)
     {
-        if(isDirect || isFully) // tag + words
-        {
-            this.offsetSize = Cache.log2(blockSize);
-            this.indexSize = Cache.log2(cacheSize);
-            this.tagSize = 32 - this.indexSize - set - this.offsetSize;
-            this.size = this.offsetSize + this.indexSize + this.tagSize;
-            
-            this.linea = new int[size];
-        }
-        
-        
+        /* funciona igual para todos los casos*/
+        this.offsetSize = Cache.log2(blockSize);
+        this.indexSize = Cache.log2(cacheSize);
+        this.tagSize = 32 - this.indexSize - set - this.offsetSize;
+        this.size = this.offsetSize + this.indexSize + this.tagSize;
+
+        this.linea = new int[size];
     }
 }
